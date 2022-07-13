4ce14876a239f32511a87cc5abde2cb59db61432
Ramisa21 <76836238+Ramisa21@users.noreply.github.com>
2022-07-14 00:55:41 +0600

    Update MainActivity.java

--- app/src/main/java/com/example/healthadvisor/MainActivity.java
+++ app/src/main/java/com/example/healthadvisor/MainActivity.java
@@ -14,8 +14,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void openRegtype(View view) {
-        startActivity(new Intent(this, RegTypeActivity.class));
-    }
-
+    s
 }
